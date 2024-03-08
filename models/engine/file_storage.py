256a4fe790915f74301fea72ce0c8b4a94d96068
Chris Halogen <christackoms@gmail.com>
2024-03-08 14:14:54 +0100

    Change The Storage Engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """returns one object based on class name and id"""
+        if cls and id:
+            fetch = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+        return all_obj.get(fetch)
+
+    def count(self, cls=None):
+        """returns the number of all objects in storage"""
+        return (len(self.all(cls)))
